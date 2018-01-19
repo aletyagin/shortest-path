ddf17c8ad48df0380f7325c5c06c9166dd052ab8
aletyagin <a.letyagin@it.ecommpay.com>
2018-01-19 15:45:59 +0300

    IS-1 fix logging

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.5-jdk-9-slim
+FROM maven:3.5-jdk-8-slim
 ADD ./ /data
 WORKDIR /data
 CMD mvn clean integration-test -Phit-refresh -Dport=8080
--- src/main/java/ru/itchyfeet/App.java
+++ src/main/java/ru/itchyfeet/App.java
@@ -1,5 +1,7 @@
 package ru.itchyfeet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.takes.Response;
 import org.takes.facets.fallback.*;
 import org.takes.facets.fork.FkMethods;
@@ -7,6 +9,7 @@ import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 import org.takes.http.*;
 import org.takes.misc.Opt;
+import org.takes.rs.RsJson;
 import org.takes.rs.RsText;
 import ru.itchyfeet.takes.Circle;
 import ru.itchyfeet.takes.TkLogged;
@@ -14,12 +17,18 @@ import ru.itchyfeet.takes.circle.algorithm.HamiltonianPath;
 
 public final class App {
     public static void main(final String... args) throws Exception {
-        new FtBasic(
+         new FtBasic(
             new BkSafe(
                 new BkParallel(
                     new BkBasic(
                         new TkFallback(
                             new TkFork(
+                                new FkMethods(
+                                    "GET",
+                                    new TkFork(
+                                        new FkRegex("/checkavailable", new RsText("1"))
+                                    )
+                                ),
                                 new FkMethods(
                                     "POST",
                                     new TkLogged(
@@ -30,13 +39,18 @@ public final class App {
                                 )
                             ),
                             new FbChain(
-                                new FbStatus(404, new RsText("sorry, page is absent")),
                                 new Fallback() {
                                     public Opt<Response> route(final RqFallback req) {
+                                        final String errorMessage = "{\"code\": 1, \"message\": \"this method is not allowed here\"}";
+
+                                        final Logger logger = LoggerFactory.getLogger("Fallback");
+                                        logger.error(errorMessage, req.throwable());
+
                                         return
                                             new Opt.Single<>(
-//                                                new RsText(ExceptionUtils.getStackTrace(req.throwable()))
-                                                new RsText("this method is not allowed here")
+                                                new RsJson(
+                                                    new RsText(errorMessage)
+                                                )
                                             );
                                     }
                                 }
